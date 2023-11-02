9d6643dbee230a420e73e36e6ca0cdd1f4c26685
andrestuestach <89998409+andrestuestach@users.noreply.github.com>
2023-11-02 18:49:55 -0500

    Update busqueda_estacionamiento.js

--- js/busqueda_estacionamiento.js
+++ js/busqueda_estacionamiento.js
@@ -3,4 +3,9 @@ let list_resul=document.getElementById("list_resul");
 
 btn_escanear.addEventListener("click",(e)=>{
     list_resul.style.display="block";
+
+    e.preventDefault();
+    list_resul.scrollIntoView({
+        behavior:"smooth"
+    });
 });
