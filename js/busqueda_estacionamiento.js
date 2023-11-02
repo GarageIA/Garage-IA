0a6caa7d494bd6eec074d7296a843939c1fce5e5
andrestuestach <89998409+andrestuestach@users.noreply.github.com>
2023-11-02 17:50:49 -0500

    Initial commit

---
+++ js/busqueda_estacionamiento.js
@@ -0,0 +1,6 @@
+let btn_escanear=document.getElementById("btn_escanear");
+let list_resul=document.getElementById("list_resul");
+
+btn_escanear.addEventListener("click",(e)=>{
+    list_resul.style.display="block";
+});
