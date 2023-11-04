024bff7d9859fd145af2a221fd69fe12b30b8146
Kevin Andres Tuesta Chirinos <andrestuesta_22@outlook.com>
2023-11-04 18:54:43 -0500

    Mover carpetaas

--- assets/js/busqueda_estacionamiento.js
+++ assets/js/busqueda_estacionamiento.js
@@ -2,6 +2,7 @@ let btn_escanear=document.getElementById("btn_escanear");
 let list_resul=document.getElementById("list_resul");
 
 
+
 btn_escanear.addEventListener("click",(e)=>{
     list_resul.style.display="block";
 
