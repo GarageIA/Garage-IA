b6ca1627903da25457d4b30bfd1567f0a5182121
andrestuestach <89998409+andrestuestach@users.noreply.github.com>
2023-11-21 14:10:00 -0500

    Update chat.js

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -1,1 +1,18 @@
+var lista_chat_output=document.querySelector(".lista-chat-output");
+var txt_comentario=document.querySelector("#txt_comentario");
 
+function Enviar(){
+    if(txt_comentario.value.trim()!=""){
+        lista_chat_output.innerHTML+='<div class="chat-output">'+
+                                '   <div class="chat-output-foto-cont">'+
+                                '       <img src="assets/images/perfil.png" alt="" srcset=""/>'+
+                                '   </div>'+
+                                '   <div class="chat-output-info">'+
+                                '       <h3>@Jeremy Doku ⭐⭐⭐⭐⭐</h3>'+
+                                '       <div class="chat-cont-txt">'+
+                                '       <p>'+txt_comentario.value+' </p>'+
+                                '   </div>'+
+                                '</div>'
+                                ;
+    }
+}
